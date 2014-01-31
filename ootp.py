94f38eded5f64ca15d135cde05d874c9f4703744
David Wen <david@dwen.me>
2014-01-31 07:50:07 -0500

    Team page

--- ootp.py
+++ ootp.py
@@ -73,13 +73,16 @@ def team(team_id):
         ''', [team_id])
     team = cur.fetchone()
 
-    cur.execute('''
-        select * from dates
-        order by id desc
-        ''')
-    date_row = cur.fetchone()
-    date_id = date_row['id']
-    date = date_row['date']
+    date_id, date = get_date()
+
+    return render_template('team.html',
+        team=team,
+        date=date)
+
+@app.route('/team/<team_id>/batting')
+def team_batting(team_id):
+    cur = g.db.cursor()
+    date_id, date = get_date()
 
     cur.execute('''
         select p.name, p.position, br.*, t.level
@@ -89,8 +92,50 @@ def team(team_id):
         join teams t on t.id = pt.team_id
         where br.date_id = ?
         and (t.id = ? or t.parent_id = ?)
+        order by level desc
         ''', [date_id, team_id, team_id])
-    batting_ratings = cur.fetchall()
+    rows = cur.fetchall()
+    ratings = {}
+    ids = []
+    for row in rows:
+        player_id = row['player_id']
+        ratings[player_id] = row
+        ids.append(player_id)
+
+    sql = '''
+        select br.*, count(*) as position
+        from batting_ratings br
+        left join batting_ratings br_later
+          on br_later.player_id = br.player_id
+          and br_later.date_id >= br.date_id
+        where br.player_id in ({seq})
+        group by br.player_id, br.date_id
+        having position = 2
+        '''.format(seq=','.join(['?']*len(ratings)))
+    cur.execute(sql, ratings.keys())
+    prev_rows = cur.fetchall()
+    prev_ratings = {}
+    diff_classes = {}
+    for row in prev_rows:
+        player_id = row['player_id']
+        prev_ratings[player_id] = row
+        diff_classes[player_id] = {}
+        for key in row.keys():
+            if row[key] < ratings[player_id][key]:
+                diff_classes[player_id][key] = 'increase'
+            elif row[key] > ratings[player_id][key]:
+                diff_classes[player_id][key] = 'decrease'
+
+    return render_template('_team_batting.html',
+        ids=ids,
+        ratings=ratings,
+        prev_ratings=prev_ratings,
+        diff_classes=diff_classes)
+
+@app.route('/team/<team_id>/pitching')
+def team_pitchers(team_id):
+    cur = g.db.cursor()
+    date_id, date = get_date()
 
     cur.execute('''
         select p.name, p.position, pr.*, t.level
@@ -100,15 +145,56 @@ def team(team_id):
         join teams t on t.id = pt.team_id
         where pr.date_id = ?
         and (t.id = ? or t.parent_id = ?)
+        order by level desc
         ''', [date_id, team_id, team_id])
-    pitching_ratings = cur.fetchall()
+    rows = cur.fetchall()
+    ratings = {}
+    ids = []
+    for row in rows:
+        player_id = row['player_id']
+        ratings[player_id] = row
+        ids.append(player_id)
 
-    return render_template('team.html',
-        team=team,
-        date=date,
-        date_id=date_id,
-        batting_ratings=batting_ratings,
-        pitching_ratings=pitching_ratings)
+    sql = '''
+        select pr.*, count(*) as position
+        from pitching_ratings pr
+        left join pitching_ratings pr_later
+          on pr_later.player_id = pr.player_id
+          and pr_later.date_id >= pr.date_id
+        where pr.player_id in ({seq})
+        group by pr.player_id, pr.date_id
+        having position = 2
+        '''.format(seq=','.join(['?']*len(ratings)))
+    cur.execute(sql, ratings.keys())
+    prev_rows = cur.fetchall()
+    prev_ratings = {}
+    diff_classes = {}
+    for row in prev_rows:
+        player_id = row['player_id']
+        prev_ratings[player_id] = row
+        diff_classes[player_id] = {}
+        for key in row.keys():
+            if row[key] < ratings[player_id][key]:
+                diff_classes[player_id][key] = 'increase'
+            elif row[key] > ratings[player_id][key]:
+                diff_classes[player_id][key] = 'decrease'
+
+    return render_template('_team_pitching.html',
+        ids=ids,
+        ratings=ratings,
+        prev_ratings=prev_ratings,
+        diff_classes=diff_classes)
+
+def get_date():
+    cur = g.db.cursor()
+    cur.execute('''
+        select * from dates
+        order by id desc
+        ''')
+    date_row = cur.fetchone()
+    date_id = date_row['id']
+    date = date_row['date']
+    return (date_id, date)
 
 if __name__ == '__main__':
     app.run(debug=True)
