ac23780960756be838947289c9ecc9644b2d61e4
David Wen <david@dwen.me>
2014-02-04 00:13:44 -0500

    Top improvers and add intangibles to player page

--- ootp.py
+++ ootp.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 DATABASE = 'wbh.db'
 ROOT = 'http://worldbaseballhierarchy.com/lgreports/news/html/'
 
+RATINGS = {
+    5: 'Very High',
+    4: 'High',
+    3: 'Normal',
+    2: 'Low',
+    1: 'Very Low'
+}
+
 def connect_db():
     db = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
@@ -24,7 +32,7 @@ def teardown_request(exception):
 def error(reason, code=400):
     return jsonify({'error': reason}), code
 
-@app.route('/player/<player_id>/')
+@app.route('/player/<int:player_id>/')
 def player(player_id):
     cur = g.db.cursor()
     cur.execute('''
@@ -37,9 +45,11 @@ def player(player_id):
     return render_template('player.html',
         player=player,
         title_href=title_href,
-        age=age)
+        age=age,
+        intelligence=RATINGS[player['intelligence']],
+        work_ethic=RATINGS[player['work_ethic']])
 
-@app.route('/player/<player_id>/batting')
+@app.route('/player/<int:player_id>/batting')
 def batting_ratings(player_id):
     cur = g.db.cursor()
     cur.execute('''
@@ -53,7 +63,7 @@ def batting_ratings(player_id):
     return render_template('_batting_ratings.html',
         rows=rows)
 
-@app.route('/player/<player_id>/pitching')
+@app.route('/player/<int:player_id>/pitching')
 def pitching_ratings(player_id):
     cur = g.db.cursor()
     cur.execute('''
@@ -80,12 +90,12 @@ def teams():
     return render_template('teams.html',
         teams=teams)
 
-@app.route('/team/<team_id>/')
+@app.route('/team/<int:team_id>/')
 def team(team_id):
     date_id, date = get_date()
     return redirect(url_for('team_date', team_id=team_id, date_id=date_id))
 
-@app.route('/team/<team_id>/date/<date_id>')
+@app.route('/team/<int:team_id>/date/<int:date_id>')
 def team_date(team_id, date_id):
     cur = g.db.cursor()
     cur.execute('select * from teams where id = ?', [team_id])
@@ -96,7 +106,7 @@ def team_date(team_id, date_id):
         date=date,
         date_id=date_id)
 
-@app.route('/team/<team_id>/date/<date_id>/batting')
+@app.route('/team/<int:team_id>/date/<int:date_id>/batting')
 def team_batting(team_id, date_id):
     sql = '''
         select p.name, p.position, p.birthday, br.*, t.level
@@ -122,7 +132,7 @@ def team_batting(team_id, date_id):
         '''
     return team_bp(team_id, date_id, sql, prev_sql, '_team_batting.html')
 
-@app.route('/team/<team_id>/date/<date_id>/pitching')
+@app.route('/team/<int:team_id>/date/<int:date_id>/pitching')
 def team_pitchers(team_id, date_id):
     sql = '''
         select p.name, p.position, p.birthday, pr.*, t.level
@@ -189,14 +199,14 @@ def improvers():
     date_id, date = get_date()
     return redirect(url_for('improvers_date', date_id=date_id))
 
-@app.route('/improvers/<date_id>')
+@app.route('/improvers/<int:date_id>')
 def improvers_date(date_id):
     date_id, date = get_date(date_id=date_id)
     return render_template('improvers.html',
         date=date,
         date_id=date_id)
 
-@app.route('/improvers/<date_id>/batting')
+@app.route('/improvers/<int:date_id>/batting')
 def improved_batting(date_id):
     max_age = int(request.args.get('maxage', '99'))
     sql = '''
@@ -225,7 +235,7 @@ def improved_batting(date_id):
         '''
     return improved_bp(date_id, max_age, sql, prev_sql, '_improvers_batting.html')
 
-@app.route('/improvers/<date_id>/pitching')
+@app.route('/improvers/<int:date_id>/pitching')
 def improved_pitching(date_id):
     max_age = int(request.args.get('maxage', '99'))
     sql = '''
@@ -293,19 +303,127 @@ def improved_bp(date_id, max_age, sql, prev_sql, template):
         diff_classes=diff_classes,
         ages=ages)
 
+@app.route('/improvers/top/')
+def top_improvers():
+    return render_template('top_improvers.html')
+
+@app.route('/improvers/top/batting')
+def top_improvers_batting():
+    max_age = int(request.args.get('maxage', '99'))
+    min_imp = int(request.args.get('minimp', '1'))
+    sql = '''
+        select
+          p.name, p.position, p.birthday, t.level, parent_team.name as ml_team, br.*,
+          br.pot_contact + br.pot_gap + br.pot_power + br.pot_eye + br.pot_avoid_k as potential
+        from batting_ratings br
+        left join batting_ratings br_later
+          on br_later.player_id = br.player_id
+          and br_later.date_id > br.date_id
+        join players p on br.player_id = p.id
+        join player_teams pt on br.player_id = pt.player_id
+        left join teams t on t.id = pt.team_id
+        left join teams parent_team on t.parent_id = parent_team.id
+        where br_later.player_id is null
+        group by br.player_id
+        '''
+    prev_sql = '''
+        select
+          p.name, p.position, p.birthday, t.level, parent_team.name as ml_team, br.*,
+          br.pot_contact + br.pot_gap + br.pot_power + br.pot_eye + br.pot_avoid_k as potential
+        from batting_ratings br
+        left join batting_ratings br_earlier
+          on br_earlier.player_id = br.player_id
+          and br_earlier.date_id < br.date_id
+        join players p on br.player_id = p.id
+        join player_teams pt on br.player_id = pt.player_id
+        left join teams t on t.id = pt.team_id
+        left join teams parent_team on t.parent_id = parent_team.id
+        where br_earlier.player_id is null
+        group by br.player_id
+        '''
+    return top_improvers_bp(max_age, min_imp, sql, prev_sql, '_improvers_batting.html')
+
+@app.route('/improvers/top/pitching')
+def top_improvers_pitching():
+    max_age = int(request.args.get('maxage', '99'))
+    min_imp = int(request.args.get('minimp', '1'))
+    sql = '''
+        select
+          p.name, p.position, p.birthday, t.level, parent_team.name as ml_team, pr.*,
+          pr.pot_stuff + pr.pot_movement + pr.pot_control as potential
+        from pitching_ratings pr
+        left join pitching_ratings pr_later
+          on pr_later.player_id = pr.player_id
+          and pr_later.date_id > pr.date_Id
+        join players p on pr.player_id = p.id
+        join player_teams pt on pr.player_id = pt.player_id
+        left join teams t on t.id = pt.team_id
+        left join teams parent_team on t.parent_id = parent_team.id
+        where pr_later.date_id is null
+        group by pr.player_id
+        '''
+    prev_sql = '''
+        select
+          p.name, p.position, p.birthday, t.level, parent_team.name as ml_team, pr.*,
+          pr.pot_stuff + pr.pot_movement + pr.pot_control as potential
+        from pitching_ratings pr
+        left join pitching_ratings pr_earlier
+          on pr_earlier.player_id = pr.player_id
+          and pr_earlier.date_id < pr.date_Id
+        join players p on pr.player_id = p.id
+        join player_teams pt on pr.player_id = pt.player_id
+        left join teams t on t.id = pt.team_id
+        left join teams parent_team on t.parent_id = parent_team.id
+        where pr_earlier.date_id is null
+        group by pr.player_id
+        '''
+    return top_improvers_bp(max_age, min_imp, sql, prev_sql, '_improvers_pitching.html')
+
+def top_improvers_bp(max_age, min_imp, sql, prev_sql, template):
+    date_id, date = get_date()
+    cur = g.db.cursor()
+    ratings = {}
+    for row in cur.execute(sql):
+        ratings[row['player_id']] = row
+    prev_ratings = {}
+    diff_classes = {}
+    ids = []
+    for prev_player in cur.execute(prev_sql):
+        player_id = prev_player['player_id']
+        player = ratings[player_id]
+        if player['potential'] > prev_player['potential']:
+            prev_ratings[player_id] = prev_player
+            diff_classes[player_id] = {}
+            for key in prev_player.keys():
+                if prev_player[key] < ratings[player_id][key]:
+                    diff_classes[player_id][key] = 'increase'
+                elif prev_player[key] > ratings[player_id][key]:
+                    diff_classes[player_id][key] = 'decrease'
+            ids.append((player['potential'] - prev_player['potential'], player['player_id']))
+    ages = get_ages(ratings, date)
+    ids = [id[1] for id in sorted(ids, key=lambda id: id[0], reverse=True) if ages[id[1]] < max_age]
+    ids = [id for id in ids if (ratings[id]['potential'] - prev_ratings[id]['potential']) >= min_imp]
+
+    return render_template(template,
+        ids=ids,
+        ratings=ratings,
+        prev_ratings=prev_ratings,
+        diff_classes=diff_classes,
+        ages=ages)
+
 @app.route('/dropped/')
 def dropped():
     date_id, date = get_date()
     return redirect(url_for('dropped_date', date_id=date_id))
 
-@app.route('/dropped/<date_id>')
+@app.route('/dropped/<int:date_id>')
 def dropped_date(date_id):
     date_id, date = get_date(date_id=date_id)
     return render_template('dropped.html',
         date=date,
         date_id=date_id)
 
-@app.route('/dropped/<date_id>/batting')
+@app.route('/dropped/<int:date_id>/batting')
 def dropped_batting(date_id):
     date_id, date = get_date(date_id=date_id)
 
@@ -332,7 +450,7 @@ def dropped_batting(date_id):
         rows=rows,
         ages=ages)
 
-@app.route('/dropped/<date_id>/pitching')
+@app.route('/dropped/<int:date_id>/pitching')
 def dropped_pitching(date_id):
     date_id, date = get_date(date_id=date_id)
 
@@ -364,14 +482,14 @@ def waivers():
     date_id, date = get_date()
     return redirect(url_for('waivers_date', date_id=date_id))
 
-@app.route('/waivers/<date_id>')
+@app.route('/waivers/<int:date_id>')
 def waivers_date(date_id):
     date_id, date = get_date(date_id=date_id)
     return render_template('waivers.html',
         date=date,
         date_id=date_id)
 
-@app.route('/waivers/<date_id>/batting')
+@app.route('/waivers/<int:date_id>/batting')
 def waivers_batting(date_id):
     date_id, date = get_date(date_id=date_id)
 
@@ -397,7 +515,7 @@ def waivers_batting(date_id):
         rows=rows,
         ages=ages)
 
-@app.route('/waivers/<date_id>/pitching')
+@app.route('/waivers/<int:date_id>/pitching')
 def waivers_pitching(date_id):
     date_id, date = get_date(date_id=date_id)
 
